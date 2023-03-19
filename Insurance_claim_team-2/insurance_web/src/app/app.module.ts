913ac5b3a4354aa2d398da097d3cf5f38a5141be
Ajil Varghese(UST <235760@ust-global.com>
2023-03-19 13:05:49 +0530

    Angularweb page

---
+++ Insurance_claim_team-2/insurance_web/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainNavComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
