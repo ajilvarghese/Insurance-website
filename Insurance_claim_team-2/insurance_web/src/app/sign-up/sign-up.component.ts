6916e82b8a142bcb01595d162114b2095222f171
Ajil Varghese(UST <235760@ust-global.com>
2023-03-21 12:44:30 +0530

    updated sign up

--- Insurance_claim_team-2/insurance_web/src/app/sign-up/sign-up.component.ts
+++ Insurance_claim_team-2/insurance_web/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+interface gender1 {
+  value: string;
+  viewValue: string;
+}
+interface city1 {
+  value: string;
+  viewValue: string;
+}
+
 
 @Component({
   selector: 'app-sign-up',
@@ -7,4 +26,25 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  gender: gender1[] = [
+    {value: 'male', viewValue: 'Male'},
+    {value: 'female', viewValue: 'Female'},
+    {value: 'other', viewValue: 'Other'},
+  ];
+  
+  city: city1[] = [
+    {value: 'Kochi', viewValue: 'Kochi'},
+    {value: 'Kozhikode', viewValue: 'Kozhikode'},
+    {value: 'Kollam', viewValue: 'Kollam'},
+    {value: 'Thrissur', viewValue: 'Thrissur'},
+    {value: 'Kannur', viewValue: 'Kannur'},
+    {value: 'Alappuzha', viewValue: 'Alappuzha'},
+    {value: 'Kottayam', viewValue: 'Kottayam'},
+  ];
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
+  hide = true;
+
 }
+
