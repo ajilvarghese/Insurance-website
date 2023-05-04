622235d6f198fd2e3b7e4924fbb841081af71200
Niranjana25 <Ninja@25>
2023-05-04 14:32:01 +0530

    new update

--- Insurance_claim_team-2/insurance_web/src/app/doctorservice.service.ts
+++ Insurance_claim_team-2/insurance_web/src/app/doctorservice.service.ts
@@ -6,6 +6,7 @@ import { Doctor } from './doctor';
 @Injectable({
   providedIn: 'root'
 })
+//service class for doctor
 export class DoctorserviceService {
   private baseUr2 = 'http://localhost:8080';
 
@@ -21,15 +22,23 @@ export class DoctorserviceService {
     };
     return this.http.get<Doctor[]>(`${this.baseUr2}/signup/doctors`);
   }
+
+//create a doctor
   createdoctor(doctor:Doctor):Observable<Object>{
     return this.http.post(`${this.baseUr2}/signup/doctors`,doctor);
   }
+
+  //get the doctor by id
   getdoctorById(doctor_id:number):Observable<Doctor>{
     return this.http.get<Doctor>(`${this.baseUr2}/signup/doctors/${doctor_id}`);
   }
+
+  //update doctor
   updatedoctor(doctor_id:number,doctor:Doctor):Observable<Object>{
     return this.http.put(`${this.baseUr2}/signup/doctors/${doctor_id}`,doctor);
   }
+
+  //delete doctor
   deletedoctor(doctor_id:number):Observable<Object>{
     return this.http.delete(`${this.baseUr2}/signup/doctors/${doctor_id}`);
   }
