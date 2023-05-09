fc4174595282ea8faf2f884a8c7742119a2bd2a0
Niranjana25 <Ninja@25>
2023-05-09 11:37:49 +0530

    modified backend

--- Insurance_claim_team-2/signup_backend/src/main/java/com/example/signup_backend/controller/provider_controller.java
+++ Insurance_claim_team-2/signup_backend/src/main/java/com/example/signup_backend/controller/provider_controller.java
@@ -22,6 +22,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/signup")
 public class provider_controller {
+    @Autowired
+    Provider_service provider_service;
+    @Autowired
+    Provider_repository provider_repository;
     private static final Logger logger = LoggerFactory.getLogger(UserNotFoundException.class);
 
     @ExceptionHandler(UserNotFoundException.class)
@@ -30,11 +34,11 @@ public class provider_controller {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), LocalDateTime.now());
         return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
     }
+
+
+
     //................Providers................
-    @Autowired
-    Provider_service provider_service;
-    @Autowired
-    Provider_repository provider_repository;
+
     @GetMapping("/providers")
 
     public List<Provider> getallProviders(){
@@ -52,13 +56,13 @@ public class provider_controller {
     //get provider by id
     @GetMapping("/providers/{provider_id}")
     public ResponseEntity<Provider> getProviderBYId(@PathVariable Long provider_id){
-        Provider provider = provider_repository.findById(provider_id).orElseThrow(() -> new UserNotFoundException("Provider does not exit !! id :"+ provider_id));
+        Provider provider = provider_repository.findById(provider_id).orElseThrow(() -> new UserNotFoundException("Provider does not exist !! id :"+ provider_id));
         return ResponseEntity.ok(provider);
     }
     //update Providers
     @PutMapping("/providers/{provider_id}")
     public ResponseEntity<Provider> updateProvider(@PathVariable Long provider_id,@RequestBody Provider providerDetails){
-        Provider provider = provider_repository.findById(provider_id).orElseThrow(() -> new UserNotFoundException("Provider does not exit !! id :"+ provider_id));
+        Provider provider = provider_repository.findById(provider_id).orElseThrow(() -> new UserNotFoundException("Provider does not exist !! id :"+ provider_id));
         provider.setHospital_clinic(providerDetails.getHospital_clinic());
         provider.setState(providerDetails.getState());
         provider.setCity(providerDetails.getCity());
@@ -69,14 +73,11 @@ public class provider_controller {
     //delete provider rest api
     @DeleteMapping("/providers/{provider_id}")
     public ResponseEntity<Map<String,Boolean>> deleteProvider(@PathVariable Long provider_id){
-        Provider provider = provider_repository.findById(provider_id).orElseThrow(() -> new UserNotFoundException("Provider does not exit !! id :"+ provider_id));
+        Provider provider = provider_repository.findById(provider_id).orElseThrow(() -> new UserNotFoundException("Provider does not exist !! id :"+ provider_id));
         provider_repository.delete(provider);
         Map<String,Boolean> response = new HashMap<>();
         response.put("Deleted",Boolean.TRUE);
         return ResponseEntity.ok(response);
     }
 
-
-
-    //.........................................
 }
